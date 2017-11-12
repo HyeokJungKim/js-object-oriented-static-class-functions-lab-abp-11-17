4d391032356be2d87c219cf82c7680670943588e
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2017-11-12 20:30:37 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,19 @@
 'use strict';
+class Question{
+  constructor(question){
+    this.content = question;
+    this.save();
+  }
+  save(){
+    this.constructor._All.push(this);
+  }
+  static All(){
+    return this._All;
+  }
+
+  static Find(number){
+    return this.All()[number-1];
+  }
+}
+
+Question._All = [];
